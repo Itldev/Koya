b5511b4892df68406e166e3fa0bb5820899e581f
chris <leroux@itldev.fr>
2014-04-30 12:16:40 +0200

    users details modification service + webscript
    partial implementation : need password modification to be complete

--- koya-alfresco/src/main/java/fr/itldev/koya/alfservice/UserService.java
+++ koya-alfresco/src/main/java/fr/itldev/koya/alfservice/UserService.java
@@ -3,33 +3,84 @@
  *
  * Copyright (C) Itl Developpement 2014
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see `<http://www.gnu.org/licenses/>`.
+ * along with this program. If not, see `<http://www.gnu.org/licenses/>`.
  */
-
-
 package fr.itldev.koya.alfservice;
 
+import fr.itldev.koya.exception.KoyaServiceException;
 import fr.itldev.koya.model.impl.User;
-import org.alfresco.repo.security.person.PersonServiceImpl;
+import fr.itldev.koya.services.exceptions.KoyaErrorCodes;
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.security.PersonService;
+import org.apache.log4j.Logger;
 
 /**
  *
  */
 public class UserService {
-    
-    public void createUser(User userLog,String mail){
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
+    protected NodeService nodeService;
+    private PersonService personService;
+
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    public void setPersonService(PersonService personService) {
+        this.personService = personService;
+    }
+
+    /**
+     * User creation method.
+     *
+     * @param userLog
+     * @param mail
+     */
+    public void createUser(User userLog, String mail) {
+
+    }
+
+    /**
+     * Modify user fields according to user object.
+     *
+     * @param userToModify
+     * @throws KoyaServiceException
+     */
+    public void modifyUser(User userToModify) throws KoyaServiceException {
         
+        //TODO check who request user modification : user can only modify his own information
+        //admin can modify everyone informations
+        
+
+        if (personService.personExists(userToModify.getLogin())) {
+            NodeRef userNr = personService.getPerson(userToModify.getLogin());
+
+            //update 4 fields : firstname,lastname,email,password            
+            nodeService.setProperty(userNr, ContentModel.PROP_FIRSTNAME, userToModify.getFirstName());
+            nodeService.setProperty(userNr, ContentModel.PROP_LASTNAME, userToModify.getName());
+            nodeService.setProperty(userNr, ContentModel.PROP_EMAIL, userToModify.getEmail());
+
+            //TODO change password if necessary + uncrypted password
+            //nodeService.setProperty(userNr, ContentModel.PROP_PASSWORD, userToModify.getPassword());
+        } else {
+            throw new KoyaServiceException(KoyaErrorCodes.UNKNOWN_USER);
+        }
+
     }
 
 }
---
+++ koya-alfresco/src/main/java/fr/itldev/koya/webscript/user/ModifyDetails.java
@@ -0,0 +1,78 @@
+/**
+ * Koya is an alfresco module that provides a corporate orientated dataroom.
+ *
+ * Copyright (C) Itl Developpement 2014
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see `<http://www.gnu.org/licenses/>`.
+ */
+package fr.itldev.koya.webscript.user;
+
+import fr.itldev.koya.alfservice.UserService;
+import fr.itldev.koya.exception.KoyaServiceException;
+import fr.itldev.koya.model.impl.User;
+import fr.itldev.koya.model.json.ItlAlfrescoServiceWrapper;
+import fr.itldev.koya.services.exceptions.KoyaErrorCodes;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+/**
+ * TODO verify authentication to allow users to modify only his personnal
+ * details. TODO admin can modify all users details.
+ */
+public class ModifyDetails extends AbstractWebScript {
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @throws IOException
+     */
+    @Override
+    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException {
+        ItlAlfrescoServiceWrapper wrapper = new ItlAlfrescoServiceWrapper();
+
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            User u = mapper.readValue(req.getContent().getReader(), User.class);
+            userService.modifyUser(u);
+
+            wrapper.setStatusOK();
+        } catch (KoyaServiceException ex) {
+            wrapper.setStatusFail(ex.toString());
+            wrapper.setErrorCode(ex.getErrorCode());
+        } catch (IOException ex) {
+            wrapper.setStatusFail(ex.toString());
+            wrapper.setErrorCode(KoyaErrorCodes.UNHANDLED);
+        }
+
+        res.setContentType("application/json");
+
+        res.getWriter().write(wrapper.getAsJSON());
+        logger.trace(wrapper.getAsJSON());
+    }
+
+}
