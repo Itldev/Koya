20d6d3d2fbd03d97a51d725ab2f8bf9e4312c211
chris <christopheleroux1@yahoo.fr>
2014-08-19 10:16:12 +0200

    BugFix : automaticly reload users preferences after favourite status modification as favourites are user's preferences, they have to be updated.

--- libkoya/src/main/java/fr/itldev/koya/services/impl/FavouriteServiceImpl.java
+++ libkoya/src/main/java/fr/itldev/koya/services/impl/FavouriteServiceImpl.java
@@ -4,14 +4,19 @@ import fr.itldev.koya.model.SecuredItem;
 import fr.itldev.koya.model.impl.User;
 import fr.itldev.koya.model.json.ItlAlfrescoServiceWrapper;
 import fr.itldev.koya.services.FavouriteService;
+import fr.itldev.koya.services.UserService;
 import fr.itldev.koya.services.exceptions.AlfrescoServiceException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FavouriteServiceImpl extends AlfrescoRestService implements FavouriteService {
-
+    
     private static final String REST_POST_TOGGLEFAVOURITE = "/s/fr/itldev/koya/global/togglefavourite";
     private static final String REST_GET_GETFAVOURITES = "/s/fr/itldev/koya/global/getfavourites";
-
+    
+    @Autowired
+    UserService userService;
+    
     @Override
     public List<SecuredItem> getFavourites(User user) throws AlfrescoServiceException {
         ItlAlfrescoServiceWrapper ret = user.getRestTemplate().getForObject(
@@ -22,7 +27,7 @@ public class FavouriteServiceImpl extends AlfrescoRestService implements Favouri
             throw new AlfrescoServiceException(ret.getMessage(), ret.getErrorCode());
         }
     }
-
+    
     @Override
     public void setFavouriteValue(User user, SecuredItem item, Boolean favouriteValue) throws AlfrescoServiceException {
         item.setUserFavourite(favouriteValue);
@@ -31,6 +36,9 @@ public class FavouriteServiceImpl extends AlfrescoRestService implements Favouri
         if (!ret.getStatus().equals(ItlAlfrescoServiceWrapper.STATUS_OK)) {
             throw new AlfrescoServiceException(ret.getMessage(), ret.getErrorCode());
         }
+        //Automaticly reload user's preferences
+        userService.loadPreferences(user);
+        
     }
-
+    
 }
