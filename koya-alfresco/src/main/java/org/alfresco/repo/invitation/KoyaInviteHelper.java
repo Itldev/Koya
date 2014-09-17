c7c1dedaa9ad49531d7183b9108a500c37b4a561
chris <christopheleroux1@yahoo.fr>
2014-09-17 15:25:47 +0200

    Define custom invite email subject from koyamail.properties file

--- koya-alfresco/src/main/java/org/alfresco/repo/invitation/KoyaInviteHelper.java
+++ koya-alfresco/src/main/java/org/alfresco/repo/invitation/KoyaInviteHelper.java
@@ -1,18 +1,105 @@
 package org.alfresco.repo.invitation;
 
+import fr.itldev.koya.alfservice.KoyaNodeService;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import org.alfresco.repo.i18n.MessageService;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarAcceptUrl;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviteTicket;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviteeGenPassword;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviteeUserName;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviterUserName;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarRejectUrl;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarResourceName;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarRole;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarServerPath;
+import org.alfresco.repo.invitation.site.InviteSender;
+import org.alfresco.repo.invitation.site.KoyaInviteSender;
+import org.alfresco.repo.jscript.ScriptNode;
+import org.alfresco.repo.model.Repository;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.workflow.WorkflowModel;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.action.ActionService;
+import org.alfresco.service.cmr.invitation.InvitationService;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.TemplateService;
+import org.alfresco.service.cmr.security.MutableAuthenticationService;
+import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.site.SiteService;
+import org.alfresco.service.cmr.workflow.WorkflowService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.util.collections.CollectionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  *
  *
  */
-public class KoyaInviteHelper extends InviteHelper {
+public class KoyaInviteHelper extends InviteHelper implements InitializingBean {
 
-    protected SiteService siteService;
+    private static final Log logger = LogFactory.getLog(ModeratedActionReject.class);
 
-    public void setSiteService(SiteService siteService) {
-        this.siteService = siteService;
+    private static final Collection<String> sendInvitePropertyNames = Arrays.asList(wfVarInviteeUserName,//
+            wfVarResourceName,//
+            wfVarInviterUserName,//
+            wfVarInviteeUserName,//
+            wfVarRole,//
+            wfVarInviteeGenPassword,//
+            wfVarResourceName,//
+            wfVarInviteTicket,//
+            wfVarServerPath,//
+            wfVarAcceptUrl,//
+            wfVarRejectUrl,
+            InviteSender.WF_INSTANCE_ID);
+
+    private Repository repositoryHelper;
+    private ServiceRegistry serviceRegistry;
+
+    private ActionService actionService;
+    private InvitationService invitationService;
+    private MutableAuthenticationService authenticationService;
+    private MessageService messageService;
+    private NamespaceService namespaceService;
+    private PersonService personService;
+    private SiteService siteService;
+    private TemplateService templateService;
+    private WorkflowService workflowService;
+    private NodeService nodeService;
+
+    private InviteSender inviteSender;
+
+    /**
+     *
+     * Koya specific property
+     */
+    private KoyaNodeService koyaNodeService;
+    private String i18nPropertiesPath;
+
+    public void setKoyaNodeService(KoyaNodeService koyaNodeService) {
+        this.koyaNodeService = koyaNodeService;
+    }
+
+    public void setI18nPropertiesPath(String i18nPropertiesPath) {
+        this.i18nPropertiesPath = i18nPropertiesPath;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        this.actionService = serviceRegistry.getActionService();
+        this.authenticationService = serviceRegistry.getAuthenticationService();
+        this.invitationService = serviceRegistry.getInvitationService();
+        this.namespaceService = serviceRegistry.getNamespaceService();
+        this.personService = serviceRegistry.getPersonService();
+        this.siteService = serviceRegistry.getSiteService();
+        this.templateService = serviceRegistry.getTemplateService();
+        this.workflowService = serviceRegistry.getWorkflowService();
+        this.nodeService = serviceRegistry.getNodeService();
+        this.inviteSender = new KoyaInviteSender(serviceRegistry, repositoryHelper, messageService,
+                koyaNodeService, i18nPropertiesPath);
     }
 
     /**
@@ -37,5 +124,50 @@ public class KoyaInviteHelper extends InviteHelper {
                 return null;
             }
         }, AuthenticationUtil.getSystemUserName());
+    }
+
+    @Override
+    public void sendNominatedInvitation(String inviteId, Map<String, Object> executionVariables) {
+        if (invitationService.isSendEmails()) {
+            Map<String, String> properties = makePropertiesFromContextVariables(executionVariables, sendInvitePropertyNames);
+
+            String packageName = WorkflowModel.ASSOC_PACKAGE.toPrefixString(namespaceService).replace(":", "_");
+            ScriptNode packageNode = (ScriptNode) executionVariables.get(packageName);
+            String packageRef = packageNode.getNodeRef().toString();
+            properties.put(InviteSender.WF_PACKAGE, packageRef);
+
+            properties.put(InviteSender.WF_INSTANCE_ID, inviteId);
+
+            inviteSender.sendMail(properties);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, String> makePropertiesFromContextVariables(Map<?, ?> executionVariables, Collection<String> propertyNames) {
+        return CollectionUtils.filterKeys((Map<String, String>) executionVariables, CollectionUtils.containsFilter(propertyNames));
+    }
+
+    /**
+     * @param messageService the messageService to set
+     */
+    @Override
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    /**
+     * @param repositoryHelper the repositoryHelper to set
+     */
+    @Override
+    public void setRepositoryHelper(Repository repositoryHelper) {
+        this.repositoryHelper = repositoryHelper;
+    }
+
+    /**
+     * @param serviceRegistry the serviceRegistry to set
+     */
+    @Override
+    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+        this.serviceRegistry = serviceRegistry;
     }
 }
---
+++ koya-alfresco/src/main/java/org/alfresco/repo/invitation/site/KoyaInviteSender.java
@@ -0,0 +1,247 @@
+package org.alfresco.repo.invitation.site;
+
+import fr.itldev.koya.alfservice.KoyaNodeService;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.action.executer.MailActionExecuter;
+import org.alfresco.repo.i18n.MessageService;
+import org.alfresco.repo.invitation.WorkflowModelNominatedInvitation;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarAcceptUrl;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviteTicket;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviteeGenPassword;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviteeUserName;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarInviterUserName;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarRejectUrl;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarResourceName;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarRole;
+import static org.alfresco.repo.invitation.WorkflowModelNominatedInvitation.wfVarServerPath;
+import static org.alfresco.repo.invitation.site.InviteSender.WF_INSTANCE_ID;
+import org.alfresco.repo.model.Repository;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ActionService;
+import org.alfresco.service.cmr.admin.RepoAdminService;
+import org.alfresco.service.cmr.invitation.InvitationException;
+import org.alfresco.service.cmr.model.FileFolderService;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.TemplateService;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.cmr.security.PersonService;
+import org.alfresco.service.cmr.site.SiteInfo;
+import org.alfresco.service.cmr.site.SiteService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.util.ModelUtil;
+import org.apache.log4j.Logger;
+import org.springframework.extensions.surf.util.ParameterCheck;
+import org.springframework.extensions.surf.util.URLEncoder;
+
+/**
+ * Ovverride invite sender in order to provide custom invite mail subject
+ *
+ */
+public class KoyaInviteSender extends InviteSender {
+
+    private static final String EMAIL_SUBJECT = "koya.invitation.invitesender.email.subject";
+    private Logger logger = Logger.getLogger(this.getClass());
+
+    // koya sepecic injections
+    private String i18nPropertiesPath;
+    private KoyaNodeService koyaNodeService;
+    //
+
+    private static final List<String> expectedProperties = Arrays.asList(wfVarInviteeUserName,//
+            WorkflowModelNominatedInvitation.wfVarResourceName,//
+            wfVarInviterUserName,//
+            wfVarInviteeUserName,//
+            WorkflowModelNominatedInvitation.wfVarRole,//
+            WorkflowModelNominatedInvitation.wfVarInviteeGenPassword,//
+            WorkflowModelNominatedInvitation.wfVarResourceName,//
+            WorkflowModelNominatedInvitation.wfVarInviteTicket,//
+            WorkflowModelNominatedInvitation.wfVarServerPath,//
+            WorkflowModelNominatedInvitation.wfVarAcceptUrl,//
+            WorkflowModelNominatedInvitation.wfVarRejectUrl, WF_INSTANCE_ID,//
+            WF_PACKAGE);
+
+    private final ActionService actionService;
+    private final NodeService nodeService;
+    private final PersonService personService;
+    private final SearchService searchService;
+    private final SiteService siteService;
+    private final Repository repository;
+    private final MessageService messageService;
+    private final FileFolderService fileFolderService;
+//    private final SysAdminParams sysAdminParams;
+    private final RepoAdminService repoAdminService;
+    private final NamespaceService namespaceService;
+
+    public KoyaInviteSender(ServiceRegistry services, Repository repository, MessageService messageService,
+            KoyaNodeService koyaNodeService, String i18nPropertiesPath) {
+
+        super(services, repository, messageService);
+        this.actionService = services.getActionService();
+        this.nodeService = services.getNodeService();
+        this.personService = services.getPersonService();
+        this.searchService = services.getSearchService();
+        this.siteService = services.getSiteService();
+        this.fileFolderService = services.getFileFolderService();
+//        this.sysAdminParams = services.getSysAdminParams();
+        this.repoAdminService = services.getRepoAdminService();
+        this.namespaceService = services.getNamespaceService();
+        this.repository = repository;
+        this.messageService = messageService;
+
+        /**
+         * Koya specific injections load
+         */
+        this.koyaNodeService = koyaNodeService;
+        this.i18nPropertiesPath = i18nPropertiesPath;
+    }
+
+    @Override
+    public void sendMail(Map<String, String> properties) {
+        checkProperties(properties);
+
+        ParameterCheck.mandatory("Properties", properties);
+        NodeRef inviter = personService.getPerson(properties.get(wfVarInviterUserName));
+        String inviteeName = properties.get(wfVarInviteeUserName);
+        NodeRef invitee = personService.getPerson(inviteeName);
+        Action mail = actionService.createAction(MailActionExecuter.NAME);
+        mail.setParameterValue(MailActionExecuter.PARAM_FROM, getEmail(inviter));
+        mail.setParameterValue(MailActionExecuter.PARAM_TO, getEmail(invitee));
+
+        /**
+         * KOYA : specific email subject
+         */
+        Properties i18n = koyaNodeService.readPropertiesFileContent(
+                koyaNodeService.getNodeRefLucenePath(i18nPropertiesPath));
+        if (i18n == null) {
+            logger.error("Invalid koya Mail properties path : " + i18nPropertiesPath);
+            return;
+        }
+
+        mail.setParameterValue(MailActionExecuter.PARAM_SUBJECT, i18n.getProperty(EMAIL_SUBJECT));
+        /**
+         *
+         */
+        mail.setParameterValue(MailActionExecuter.PARAM_SUBJECT_PARAMS, new Object[]{
+            ModelUtil.getProductName(repoAdminService), getSiteName(properties)});
+        mail.setParameterValue(MailActionExecuter.PARAM_TEMPLATE, getEmailTemplateNodeRef());
+        mail.setParameterValue(MailActionExecuter.PARAM_TEMPLATE_MODEL,
+                (Serializable) buildMailTextModel(properties, inviter, invitee));
+        mail.setParameterValue(MailActionExecuter.PARAM_IGNORE_SEND_FAILURE, true);
+        actionService.executeAction(mail, getWorkflowPackage(properties));
+    }
+
+    /**
+     * @param properties
+     */
+    private void checkProperties(Map<String, String> properties) {
+        Set<String> keys = properties.keySet();
+        if (!keys.containsAll(expectedProperties)) {
+            LinkedList<String> missingProperties = new LinkedList<>(expectedProperties);
+            missingProperties.removeAll(keys);
+            throw new InvitationException("The following mandatory properties are missing:\n" + missingProperties);
+        }
+    }
+
+    private String getEmail(NodeRef person) {
+        return (String) nodeService.getProperty(person, ContentModel.PROP_EMAIL);
+    }
+
+    private NodeRef getEmailTemplateNodeRef() {
+        List<NodeRef> nodeRefs = searchService.selectNodes(repository.getRootHome(),
+                "app:company_home/app:dictionary/app:email_templates/cm:invite/cm:invite-email.html.ftl", null,
+                this.namespaceService, false);
+
+        if (nodeRefs.size() == 1) {
+            // Now localise this
+            NodeRef base = nodeRefs.get(0);
+            NodeRef local = fileFolderService.getLocalizedSibling(base);
+            return local;
+        } else {
+            throw new InvitationException("Cannot find the email template!");
+        }
+    }
+
+    private Map<String, Serializable> buildMailTextModel(Map<String, String> properties, NodeRef inviter, NodeRef invitee) {
+        // Set the core model parts
+        // Note - the user part is skipped, as that's implied via the run-as
+        Map<String, Serializable> model = new HashMap<String, Serializable>();
+        model.put(TemplateService.KEY_COMPANY_HOME, repository.getCompanyHome());
+        model.put(TemplateService.KEY_USER_HOME, repository.getUserHome(repository.getPerson()));
+        model.put(TemplateService.KEY_PRODUCT_NAME, ModelUtil.getProductName(repoAdminService));
+
+        // Build up the args for rendering inside the template
+        Map<String, String> args = buildArgs(properties, inviter, invitee);
+        model.put("args", (Serializable) args);
+
+        // All done
+        return model;
+    }
+
+    private NodeRef getWorkflowPackage(Map<String, String> properties) {
+        String packageRef = properties.get(WF_PACKAGE);
+        return new NodeRef(packageRef);
+    }
+
+    private String getSiteName(Map<String, String> properties) {
+        String siteFullName = properties.get(wfVarResourceName);
+        SiteInfo site = siteService.getSite(siteFullName);
+        if (site == null) {
+            throw new InvitationException("The site " + siteFullName + " could not be found.");
+        }
+
+        String siteName = site.getShortName();
+        String siteTitle = site.getTitle();
+        if (siteTitle != null && siteTitle.length() > 0) {
+            siteName = siteTitle;
+        }
+        return siteName;
+    }
+
+    private Map<String, String> buildArgs(Map<String, String> properties, NodeRef inviter, NodeRef invitee) {
+        String params = buildUrlParamString(properties);
+        String acceptLink = makeLink(properties.get(wfVarServerPath), properties.get(wfVarAcceptUrl), params);
+        String rejectLink = makeLink(properties.get(wfVarServerPath), properties.get(wfVarRejectUrl), params);
+
+        Map<String, String> args = new HashMap<String, String>();
+        args.put("inviteePersonRef", invitee.toString());
+        args.put("inviterPersonRef", inviter.toString());
+        args.put("siteName", getSiteName(properties));
+        args.put("inviteeSiteRole", getRoleName(properties));
+        args.put("inviteeUserName", properties.get(wfVarInviteeUserName));
+        args.put("inviteeGenPassword", properties.get(wfVarInviteeGenPassword));
+        args.put("acceptLink", acceptLink);
+        args.put("rejectLink", rejectLink);
+        return args;
+    }
+
+    private String buildUrlParamString(Map<String, String> properties) {
+        StringBuilder params = new StringBuilder("?inviteId=");
+        params.append(properties.get(WF_INSTANCE_ID));
+        params.append("&inviteeUserName=");
+        params.append(URLEncoder.encode(properties.get(wfVarInviteeUserName)));
+        params.append("&siteShortName=");
+        params.append(properties.get(wfVarResourceName));
+        params.append("&inviteTicket=");
+        params.append(properties.get(wfVarInviteTicket));
+        return params.toString();
+    }
+
+    private String getRoleName(Map<String, String> properties) {
+        String roleName = properties.get(wfVarRole);
+        String role = messageService.getMessage("invitation.invitesender.email.role." + roleName);
+        if (role == null) {
+            role = roleName;
+        }
+        return role;
+    }
+}
