64d9dec22eefc1446f1c271ff3e973dbf789b1f0
chris <christopheleroux1@yahoo.fr>
2015-03-17 17:16:56 +0100

    throw error for all unzip exception

--- koya-alfresco/src/main/java/fr/itldev/koya/webscript/content/ImportZip.java
+++ koya-alfresco/src/main/java/fr/itldev/koya/webscript/content/ImportZip.java
@@ -18,21 +18,14 @@
  */
 package fr.itldev.koya.webscript.content;
 
-import fr.itldev.koya.action.UnzipActionExecuter;
-import fr.itldev.koya.alfservice.KoyaContentService;
-import fr.itldev.koya.alfservice.KoyaNodeService;
-import fr.itldev.koya.exception.KoyaServiceException;
-import fr.itldev.koya.services.exceptions.KoyaErrorCodes;
-import fr.itldev.koya.webscript.KoyaWebscript;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.alfresco.repo.action.executer.ImporterActionExecuter;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ActionService;
-import org.alfresco.service.cmr.model.FileFolderService;
-import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.apache.log4j.Logger;
@@ -41,73 +34,81 @@ import org.springframework.extensions.webscripts.WebScriptException;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
 
+import fr.itldev.koya.action.UnzipActionExecuter;
+import fr.itldev.koya.alfservice.KoyaNodeService;
+import fr.itldev.koya.exception.KoyaServiceException;
+import fr.itldev.koya.services.exceptions.KoyaErrorCodes;
+import fr.itldev.koya.webscript.KoyaWebscript;
+
 /**
- *
+ * 
  * Import Zip Webscript
- *
- *
- *
+ * 
+ * 
+ * 
  */
 public class ImportZip extends AbstractWebScript {
 
-    private final Logger logger = Logger.getLogger(ImportZip.class);
+	private final Logger logger = Logger.getLogger(ImportZip.class);
 
-    protected NodeService nodeService;
-    protected ActionService actionService;
+	protected NodeService nodeService;
+	protected ActionService actionService;
 
-    /*services*/
-    private KoyaNodeService koyaNodeService;
+	/* services */
+	private KoyaNodeService koyaNodeService;
 
-    public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
-    }
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
 
-    public void setActionService(ActionService actionService) {
-        this.actionService = actionService;
-    }
+	public void setActionService(ActionService actionService) {
+		this.actionService = actionService;
+	}
 
-    
-    public void setKoyaNodeService(KoyaNodeService koyaNodeService) {
-        this.koyaNodeService = koyaNodeService;
-    }
+	public void setKoyaNodeService(KoyaNodeService koyaNodeService) {
+		this.koyaNodeService = koyaNodeService;
+	}
 
-    @Override
-    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException {
-        Map<String, String> urlParamsMap = KoyaWebscript.getUrlParamsMap(req);
+	@Override
+	public void execute(WebScriptRequest req, WebScriptResponse res)
+			throws IOException {
+		Map<String, String> urlParamsMap = KoyaWebscript.getUrlParamsMap(req);
 
-        try {
-            NodeRef zipNodeRef = koyaNodeService.getNodeRef((String) urlParamsMap.get(KoyaWebscript.WSCONST_NODEREF));
+		try {
+			NodeRef zipNodeRef = koyaNodeService
+					.getNodeRef((String) urlParamsMap
+							.get(KoyaWebscript.WSCONST_NODEREF));
 
-            importZip(zipNodeRef);
-        } catch (KoyaServiceException ex) {
-            throw new WebScriptException("KoyaError : " + ex.getErrorCode().toString());
-        }
-        res.setContentType("application/json");
-        res.getWriter().write("");
-    }
+			importZip(zipNodeRef);
+		} catch (KoyaServiceException ex) {
+			throw new WebScriptException("KoyaError : "
+					+ ex.getErrorCode().toString());
+		}
+		res.setContentType("application/json");
+		res.getWriter().write("");
+	}
 
-    private void importZip(NodeRef zipFile) throws KoyaServiceException {
-        try {
-            Map<String, Serializable> paramsImport = new HashMap<>();
-            paramsImport.put(ImporterActionExecuter.PARAM_DESTINATION_FOLDER, nodeService.getPrimaryParent(zipFile).getParentRef());
-            Action importZip = actionService.createAction(UnzipActionExecuter.NAME, paramsImport);
-//            /**
-//             * Process must be executed synchronously in order to delete
-//             * original zip
-//             *
-//             * We could also have written a new action that exctracts AND delete
-//             * zip.
-//             */
-            importZip.setExecuteAsynchronously(false);
-            actionService.executeAction(importZip, zipFile);
-        } catch (InvalidNodeRefException ex) {
-            throw new KoyaServiceException(KoyaErrorCodes.ZIP_EXTRACTION_PROCESS_ERROR, ex);
-        }
-//
-//        try {
-//            fileFolderService.delete(zipFile);
-//        } catch (Exception ex) {
-//            throw new KoyaServiceException(KoyaErrorCodes.FILE_DELETE_ERROR, ex);
-//        }
-    }
+	private void importZip(NodeRef zipFile) throws KoyaServiceException {
+		try {
+			Map<String, Serializable> paramsImport = new HashMap<>();
+			paramsImport.put(ImporterActionExecuter.PARAM_DESTINATION_FOLDER,
+					nodeService.getPrimaryParent(zipFile).getParentRef());
+			Action importZip = actionService.createAction(
+					UnzipActionExecuter.NAME, paramsImport);
+			// /**
+			// * Process must be executed synchronously in order to delete
+			// * original zip
+			// *
+			// * We could also have written a new action that extracts AND
+			// delete
+			// * zip.
+			// */
+			importZip.setExecuteAsynchronously(false);
+			actionService.executeAction(importZip, zipFile);
+		} catch (Exception ex) {
+			throw new KoyaServiceException(
+					KoyaErrorCodes.ZIP_EXTRACTION_PROCESS_ERROR, ex);
+		}
+
+	}
 }
